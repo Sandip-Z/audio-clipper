fd1152e5b2dc0198fd42c7a5227e2b6e4d460e14
sandipsatyal <sandipsatyal25@gmail.com>
2021-09-06 22:16:57 +0545

    add: play and pause on selectedTimeSlot

--- src/Components/Player/default.js
+++ src/Components/Player/default.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const RemoveSource = ({ removeSource }) => {
   return <button onClick={removeSource}>Remove</button>;
 };
 
-const DefaultPlayer = ({ className, src, handleFileUpload, removeSource }) => {
-  if (src) {
-    return (
-      <>
-        <audio controls className={className}>
-          <source src={src} />
-        </audio>
-        <RemoveSource removeSource={removeSource} />
-      </>
-    );
+const DefaultPlayer = React.memo(
+  ({ className, src, handleFileUpload, removeSource, selectedTimeSlot }) => {
+    const audioRef = useRef(null);
+
+    const handleOnPlay = () => {
+      if (
+        Math.trunc(audioRef?.current?.currentTime) === selectedTimeSlot.endTime
+      ) {
+        audioRef.current.pause();
+        audioRef.current.currentTime = selectedTimeSlot.startTime;
+      }
+    };
+
+    useEffect(() => {
+      if (audioRef.current) {
+        audioRef.current.currentTime = selectedTimeSlot.startTime;
+      }
+    });
+
+    if (src) {
+      return (
+        <>
+          <audio
+            controls
+            className={className}
+            ref={audioRef}
+            onTimeUpdate={handleOnPlay}
+          >
+            <source src={src} />
+          </audio>
+          <RemoveSource removeSource={removeSource} />
+        </>
+      );
+    }
+    return <input type="file" onChange={handleFileUpload} accept="audio/*" />;
   }
-  return <input type="file" onChange={handleFileUpload} accept="audio/*" />;
-};
+);
 
 export default DefaultPlayer;
