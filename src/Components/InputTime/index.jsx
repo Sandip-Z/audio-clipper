fb4a8b44c8ac19fdcf641f34db4548d531ad4a34
sandipsatyal <sandipsatyal25@gmail.com>
2021-09-10 00:02:05 +0545

    remove: use timeslot service to calculate the time of inputs instead of storing them in states

--- src/Components/InputTime/index.jsx
+++ src/Components/InputTime/index.jsx
@@ -1,32 +1,65 @@
-import {useState, useEffect} from 'react'
+import {TimeSlotService} from '../../Services/TimeSlotService'
 
 const InputTime = ({id, time, handleTimeChange, type}) => {
-    const [hour, setHour] = useState(0);
-    const [minute, setMinute] = useState(0);
-    const [second, setSecond] = useState(0);
 
-
-    useEffect(() => {
-        const hours = time / 60 / 60;
-        const minutes = (time / 60 ) % 60;
-        const seconds = (time % 60);
-        setHour(Math.trunc(hours));
-        setMinute(Math.trunc(minutes))
-        setSecond(Math.trunc(seconds))
-    },[time])
-
-    useEffect(() => {
-        const hrInSec = hour * 60 *60;
-        const minInSec = minute * 60;
-        const changedTime = hrInSec + minInSec + second;
-        handleTimeChange(id, type, changedTime)
-    }, [hour, minute, second])
-
+    const { 
+        GetHoursFromSeconds, 
+        GetLeftOverSecondsFromSeconds, 
+        GetMinutesFromSeconds,
+        GetChangedTimeToUpdateTimeFromInputs,
+    } = TimeSlotService
 
     return <>
-        <input type="number" id="customTimeComponent-Hour" value={hour} onChange={(event)=>setHour(event.target.value)}/>
-        <input type="number" id="customTimeComponent-Minute" value={minute} onChange={(event) =>setMinute(event.target.value)} max={59}/>
-        <input type="number" id="customTimeComponent-Second" value={second} onChange={(event)=>setSecond(parseInt(event.target.value))} max={59} />
+        <input 
+            type="number" 
+            id="customTimeComponent-Hour" 
+            value={GetHoursFromSeconds(time)} 
+            onChange={(event)=>handleTimeChange(
+                id, 
+                type, 
+                GetChangedTimeToUpdateTimeFromInputs(
+                    {
+                        type: "hour", 
+                        value: event.target.value, 
+                        seconds: time
+                    }
+                    ))}
+            min={0}
+        />
+        <input 
+            type="number" 
+            id="customTimeComponent-Minute" 
+            value={GetMinutesFromSeconds(time)} 
+            onChange={(event) =>handleTimeChange(
+                id, 
+                type, 
+                GetChangedTimeToUpdateTimeFromInputs(
+                    {
+                        type: "minute", 
+                        value: event.target.value, 
+                        seconds: time
+                    }
+                    ))}
+            max={59}
+            min={0}
+        />
+        <input 
+            type="number" 
+            id="customTimeComponent-Second" 
+            value={GetLeftOverSecondsFromSeconds(time)} 
+            onChange={(event)=>handleTimeChange(
+                id, 
+                type, 
+                GetChangedTimeToUpdateTimeFromInputs(
+                    {
+                        type: "second", 
+                        value: parseInt(event.target.value), 
+                        seconds: time
+                    }
+                    ))} 
+            max={59} 
+            min={0}
+        />
     </>
 }
 
