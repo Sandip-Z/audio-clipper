e0eecbbdb5ede0798f16d3c77a99381af4ab3862
sandipsatyal <sandipsatyal25@gmail.com>
2021-09-11 23:18:34 +0545

    seperate component for timeslot

---
+++ src/Components/Organism/Timeslot/index.jsx
@@ -0,0 +1,74 @@
+import {
+    TiDelete,
+    TiDownload,
+    TiMediaPlay,
+    TiMediaPause,
+  } from "react-icons/ti";
+
+import InputTime from "Components/Molecules/InputTime";
+
+const TimeSlot = ({
+    timeslot, 
+    handlePlaySelectedTimeSlot, 
+    handleSelectedTimeSlotChanged, 
+    selectedTimeSlot, 
+    handleSelectedTimeSlotTimeChanged, 
+    deleteTimeSlot
+  }) => {
+    return (
+            <div className="timeSlots__wrapper" key={`timeSlots${timeslot.id}`}>
+              <div className="timeSlots__wrapper--checkboxWrapper">
+                <input
+                  type="checkbox"
+                  checked={timeslot.id === selectedTimeSlot?.id ? true : false}
+                  onChange={() => handleSelectedTimeSlotChanged(timeslot.id)}
+                  title={
+                    timeslot.id === selectedTimeSlot?.id
+                      ? "Selected Time Slot"
+                      : "Select this time slot to play"
+                  }
+                />
+              </div>
+              <div className="timeSlots__wrapper--inputAndButtonWrapper">
+                <div className="timeSlots__wrapper--inputWrapper">
+                  From:
+                  <InputTime
+                    key={`from-${timeslot.id}`}
+                    type="startTime"
+                    time={timeslot.startTime}
+                    handleTimeChange={handleSelectedTimeSlotTimeChanged}
+                    id={timeslot.id}
+                  />
+                  To:{" "}
+                  <InputTime
+                    key={`to-${timeslot.id}`}
+                    type="endTime"
+                    time={timeslot.endTime}
+                    id={timeslot.id}
+                    handleTimeChange={handleSelectedTimeSlotTimeChanged}
+                  />
+                </div>
+                <div className="timeSlots__wrapper--buttonWrapper">
+                  <button
+                    onClick={() => deleteTimeSlot(timeslot.id)}
+                    title="Delete this time slot"
+                    className="clip-button"
+                  >
+                    <TiDelete size={22} color={"#d63031"} />
+                  </button>
+                  <button disabled={true} className="clip-button">
+                    <TiDownload size={22} color={"#0984e3"} />
+                  </button>
+                  <button
+                    onClick={() => handlePlaySelectedTimeSlot(timeslot.id)}
+                    className="clip-button"
+                  >
+                    <TiMediaPlay size={22} color={"#00b894"} />
+                  </button>
+                </div>
+              </div>
+        </div>
+    )
+  }
+
+export default TimeSlot
